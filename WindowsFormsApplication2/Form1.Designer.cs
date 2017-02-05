c591d2a246c536635765c7f16ab1b4a19a94045a
André Assunção <theconehard@gmail.com>
2017-02-05 03:18:57 -0200

    First Commit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comoUsarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verCompEditorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eGENDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.ajudaToolStripMenuItem,
+            this.sobreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(640, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.salvarComoToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // salvarComoToolStripMenuItem
+            // 
+            this.salvarComoToolStripMenuItem.Name = "salvarComoToolStripMenuItem";
+            this.salvarComoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.salvarComoToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.salvarComoToolStripMenuItem.Text = "Salvar Como";
+            this.salvarComoToolStripMenuItem.Click += new System.EventHandler(this.salvarComoToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.comoUsarToolStripMenuItem});
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.ajudaToolStripMenuItem.Text = "Ajuda";
+            // 
+            // comoUsarToolStripMenuItem
+            // 
+            this.comoUsarToolStripMenuItem.Name = "comoUsarToolStripMenuItem";
+            this.comoUsarToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.J)));
+            this.comoUsarToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.comoUsarToolStripMenuItem.Text = "Como usar";
+            this.comoUsarToolStripMenuItem.Click += new System.EventHandler(this.comoUsarToolStripMenuItem_Click);
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.verCompEditorToolStripMenuItem,
+            this.eGENDToolStripMenuItem});
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // verCompEditorToolStripMenuItem
+            // 
+            this.verCompEditorToolStripMenuItem.Name = "verCompEditorToolStripMenuItem";
+            this.verCompEditorToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.verCompEditorToolStripMenuItem.Text = "VerComp Editor";
+            this.verCompEditorToolStripMenuItem.Click += new System.EventHandler(this.verCompEditorToolStripMenuItem_Click);
+            // 
+            // eGENDToolStripMenuItem
+            // 
+            this.eGENDToolStripMenuItem.Name = "eGENDToolStripMenuItem";
+            this.eGENDToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.eGENDToolStripMenuItem.Text = "EGEND";
+            this.eGENDToolStripMenuItem.Click += new System.EventHandler(this.eGENDToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Selecionar pasta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(430, 221);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 109);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(436, 79);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Gerar Ver";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(147, 277);
+            this.listBox1.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(165, 53);
+            this.textBox3.MaxLength = 999999999;
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(254, 277);
+            this.textBox3.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Arquivos carregados:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(165, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "VerComp Preview:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(433, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Número de arquivos: 0";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 340);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "VerComp Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarComoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comoUsarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verCompEditorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eGENDToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication2
+{
+   
+    public partial class Form1 : Form
+    {
+        public static string diretorio = "";
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void salvarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Arquivo dat | *.dat";
+            sfd.FileName = "VerComp.dat";
+            if (File.Exists(diretorio + "\\VerComp.dat")|| File.Exists(diretorio + "\\ver"))
+            {
+                try
+                {
+                    File.Delete(diretorio + "\\VerComp.dat");
+                    File.Delete(diretorio + "\\ver");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+            }
+            sfd.ShowDialog();
+            
+            if (string.IsNullOrEmpty(sfd.FileName)==false)
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                    {
+                        writer.WriteLine(diretorio);
+                        writer.WriteLine(textBox3.Text);
+                        writer.Flush();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(string.Format("Não foi possível salvar o arquivo. Erro: {0}", ex.Message),"Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox3.Text = "";
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                listBox1.HorizontalScrollbar = true;
+                StringBuilder sb = new StringBuilder();
+                String[] allfiles = Directory.GetFiles(fbd.SelectedPath, "*.*", SearchOption.AllDirectories);
+
+                foreach (string file in allfiles)
+                {
+                    long info = new FileInfo(file).Length;
+                    using (FileStream arquivo = new FileStream(file, FileMode.Open)) {
+                        HashAlgorithm hash;
+                        hash = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                        if (hash != null)
+                        {
+                            byte[] retVal = hash.ComputeHash(arquivo);
+                            for (int i=0; i< retVal.Length; i++)
+                            {
+                                int k=0;
+                                k++;
+                                if (k < 32)
+                                {
+                                    sb.Append(retVal[i].ToString("x2"));
+                                }                                
+                            }
+                            diretorio = fbd.SelectedPath+"\\";
+                            textBox3.AppendText(Path.GetFullPath(file) + "," + sb.ToString() + "," + info + "\n");
+                            textBox3.Text = textBox3.Text.Replace(diretorio, "");
+                            textBox3.Text = textBox3.Text.Replace("\\", "/");
+                            textBox3.Text = textBox3.Text.Replace(diretorio + "/", "");
+                            sb.Clear();
+                        }
+                        hash.Dispose();
+                    }  
+                        listBox1.Items.Add(file);
+                        int numfile = listBox1.Items.Count;
+                        label1.Text = "Número de arquivos: " + numfile;
+                }
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Arquivo dat | *.dat";
+            ofd.ShowDialog();
+            listBox1.Enabled = false;
+            if (string.IsNullOrEmpty(ofd.FileName) == false)
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(ofd.FileName, Encoding.GetEncoding(CultureInfo.GetCultureInfo("pt-BR").TextInfo.ANSICodePage)))
+                    {
+                        textBox3.Text = reader.ReadToEnd();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(string.Format("Não foi possível abrir o arquivo. Erro: {0}", ex.Message), "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Title = "Selecione o arquivo";
+                ofd.ShowDialog();
+                StringBuilder sb = new StringBuilder();
+                using (FileStream arquivo = new FileStream(ofd.FileName, FileMode.Open))
+                {
+                    HashAlgorithm hash;
+                    hash = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                    if (hash != null)
+                    {
+                        byte[] retVal = hash.ComputeHash(arquivo);
+                        for (int i = 0; i < retVal.Length; i++)
+                        {
+                                sb.Append(retVal[i].ToString("x2"));
+                        }
+                    }
+                    hash.Dispose();
+                    string verhash = sb.ToString();
+                    SaveFileDialog sfd = new SaveFileDialog();
+                    sfd.FileName = "ver";
+                    sfd.ShowDialog();
+
+                    if (string.IsNullOrEmpty(sfd.FileName) == false)
+                    {
+                        try
+                        {
+                            using (StreamWriter writer = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                            {
+                                writer.WriteLine(verhash);
+                                writer.Flush();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(string.Format("Não foi possível salvar o arquivo. Erro: {0}", ex.Message), "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Não foi possível abrir o arquivo. Erro: {0}", ex.Message), "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comoUsarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format(" Criação do VerComp \n NÃO DEIXE NENHUM arquivo como somente leitura \n 1 - Selecione a pasta com os arquivos de atualização.\n 2 - Salve o arquivo (Arquivo -> Salvar Como ou Ctrl+S).\n -------------------------------------------------------------------\n Criação do Ver \n 1 - Clique em Gerar Ver. \n 2 - Selecione o VerComp.dat criado anteriormente. \n 3 - Salve seu Ver."), "Como Usar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void eGENDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("A EGEND é uma empresa de tecnologia, especializada no desenvolvimento e distribuição de produtos digitais, buscamos unir eficiência e simplicidade em todos nossos projetos para que todos os produtos sejam acessíveis a qualquer tipo de pessoa."), "Sobre a EGEND", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void verCompEditorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("Versão 1.4"), "Sobre o VerComp Editor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            Process.Start("http://egendnetwork.com/");
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
